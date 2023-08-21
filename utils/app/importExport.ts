a0f4a1886eaf46d2bf3552dad9ad55d8f88856fb
tylerbry <tylergibbs049@gmail.com>
2023-08-21 12:00:08 -0500

    Initial commit

---
+++ utils/app/importExport.ts
@@ -0,0 +1,171 @@
+import { Conversation } from '@/types/chat';
+import {
+  ExportFormatV1,
+  ExportFormatV2,
+  ExportFormatV3,
+  ExportFormatV4,
+  LatestExportFormat,
+  SupportedExportFormats,
+} from '@/types/export';
+import { FolderInterface } from '@/types/folder';
+import { Prompt } from '@/types/prompt';
+
+
+import { cleanConversationHistory } from './clean';
+
+export function isExportFormatV1(obj: any): obj is ExportFormatV1 {
+  return Array.isArray(obj);
+}
+
+export function isExportFormatV2(obj: any): obj is ExportFormatV2 {
+  return !('version' in obj) && 'folders' in obj && 'history' in obj;
+}
+
+export function isExportFormatV3(obj: any): obj is ExportFormatV3 {
+  return obj.version === 3;
+}
+
+export function isExportFormatV4(obj: any): obj is ExportFormatV4 {
+  return obj.version === 4;
+}
+
+export const isLatestExportFormat = isExportFormatV4;
+
+export function cleanData(data: SupportedExportFormats): LatestExportFormat {
+  if (isExportFormatV1(data)) {
+    return {
+      version: 4,
+      history: cleanConversationHistory(data),
+      folders: [],
+      prompts: [],
+    };
+  }
+
+  if (isExportFormatV2(data)) {
+    return {
+      version: 4,
+      history: cleanConversationHistory(data.history || []),
+      folders: (data.folders || []).map((chatFolder) => ({
+        id: chatFolder.id.toString(),
+        name: chatFolder.name,
+        type: 'chat',
+      })),
+      prompts: [],
+    };
+  }
+
+  if (isExportFormatV3(data)) {
+    return { ...data, version: 4, prompts: [] };
+  }
+
+  if (isExportFormatV4(data)) {
+    return data;
+  }
+
+  throw new Error('Unsupported data format');
+}
+
+
+
+
+import { Document, Paragraph, Packer, TextRun } from 'docx';
+
+export const exportData = (message: any) => {
+  // Define the content for the Word document
+  const content = [];
+
+
+
+  // Convert the message data to paragraphs and add to the content array
+  
+  content.push(new Paragraph(`${message.content}`));
+
+  // Create the Word document with the content
+  const doc = new Document({
+      sections: [{
+          properties: {},
+          children: content
+      }],
+  });
+
+  // Generate a blob for the .docx file
+  Packer.toBlob(doc).then(blob => {
+      // Create a URL for the blob
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.download = `assistant_response_${new Date().toISOString()}.docx`;
+      link.href = url;
+      link.style.display = 'none';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+  });
+};
+
+
+// Function to get the current date for the filename
+const currentDate = () => {
+    const date = new Date();
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+};
+
+
+
+
+
+
+
+export const importData = (
+  data: SupportedExportFormats,
+): LatestExportFormat => {
+  const { history, folders, prompts } = cleanData(data);
+
+  const oldConversations = localStorage.getItem('conversationHistory');
+  const oldConversationsParsed = oldConversations
+    ? JSON.parse(oldConversations)
+    : [];
+
+  const newHistory: Conversation[] = [
+    ...oldConversationsParsed,
+    ...history,
+  ].filter(
+    (conversation, index, self) =>
+      index === self.findIndex((c) => c.id === conversation.id),
+  );
+  localStorage.setItem('conversationHistory', JSON.stringify(newHistory));
+  if (newHistory.length > 0) {
+    localStorage.setItem(
+      'selectedConversation',
+      JSON.stringify(newHistory[newHistory.length - 1]),
+    );
+  } else {
+    localStorage.removeItem('selectedConversation');
+  }
+
+  const oldFolders = localStorage.getItem('folders');
+  const oldFoldersParsed = oldFolders ? JSON.parse(oldFolders) : [];
+  const newFolders: FolderInterface[] = [
+    ...oldFoldersParsed,
+    ...folders,
+  ].filter(
+    (folder, index, self) =>
+      index === self.findIndex((f) => f.id === folder.id),
+  );
+  localStorage.setItem('folders', JSON.stringify(newFolders));
+
+  const oldPrompts = localStorage.getItem('prompts');
+  const oldPromptsParsed = oldPrompts ? JSON.parse(oldPrompts) : [];
+  const newPrompts: Prompt[] = [...oldPromptsParsed, ...prompts].filter(
+    (prompt, index, self) =>
+      index === self.findIndex((p) => p.id === prompt.id),
+  );
+  localStorage.setItem('prompts', JSON.stringify(newPrompts));
+
+  return {
+    version: 4,
+    history: newHistory,
+    folders: newFolders,
+    prompts: newPrompts,
+  };
+};
